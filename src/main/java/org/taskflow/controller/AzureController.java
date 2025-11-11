4636fb64b4d02f9b704afef35dbd184bcd11bc54
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-11-11 15:22:17 +0100

    Add HTTP configuration and hello endpoint to AzureController

--- src/main/java/org/taskflow/controller/AzureController.java
+++ src/main/java/org/taskflow/controller/AzureController.java
@@ -1,11 +1,13 @@
 package org.taskflow.controller;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
 @Path("/")
 public class AzureController {
 
     @Path("/hello")
+    @GET
     public String hello() {
         return "Hello, Azure!";
     }
